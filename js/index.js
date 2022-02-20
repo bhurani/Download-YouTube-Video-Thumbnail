c19c73b6caa799bb2a61fccf5f91032dc41810f5
Mayank Bhurani <99683515+bhurani@users.noreply.github.com>
2022-02-20 22:25:09 +0530

    Update index.js

---
+++ js/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const urlField = document.querySelector(".url-field"),
+      hiddenField = document.querySelector(".hidden-field"),
+      previewArea = document.querySelector(".preview-area"),
+      imgTag = previewArea.querySelector(".thumbnail"),
+      button = document.querySelector(".download-btn");
+
+urlField.onkeyup = () =>{
+  let imgUrl = urlField.value;
+  previewArea.classList.add("active");
+  button.style.pointerEvents = "auto";
+
+  if(imgUrl.indexOf("https://www.youtube.com/watch?v=") != -1) {
+    let vidId = imgUrl.split('v=')[1].substring(0, 11),
+        ytImgUrl = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
+    imgTag.src = ytImgUrl;
+  }else if(imgUrl.indexOf("https://youtu.be/") != -1) {
+    let vidId = imgUrl.split('be/')[1].substring(0, 11),
+        ytImgUrl = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
+    imgTag.src = ytImgUrl;
+  }else if(imgUrl.match(/\.(jpe?g|png|gif|bmp|webp)$/i)) {
+    imgTag.src = imgUrl;
+  }else {
+    imgTag.src = "";
+    button.style.pointerEvents = "none";
+    previewArea.classList.remove("active");
+  }
+
+  hiddenField.value = imgTag.src;
+};
