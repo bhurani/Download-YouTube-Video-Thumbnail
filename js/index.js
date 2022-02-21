84582e463d7d424e9b1d923221f4c9dee18ab6fd
Mayank Bhurani <99683515+bhurani@users.noreply.github.com>
2022-02-22 00:34:19 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,35 +1,34 @@
-"use strict";
+'use strict';
 
-const urlField = document.querySelector(".url-field"),
-      hiddenField = document.querySelector(".hidden-field"),
-      previewArea = document.querySelector(".preview-area"),
-      imgTag = previewArea.querySelector(".thumbnail"),
-      button = document.querySelector(".download-btn");
+const form = document.forms[0],
+      urlField = form.querySelector('.url-field'),
+      hiddenField = form.querySelector('.hidden-field'),
+      previewArea = form.querySelector('.preview-area'),
+      imgTag = previewArea.querySelector('.thumbnail'),
+      button = form.querySelector('.download-btn');
 
 urlField.onkeyup = () =>{
   let imgUrl = urlField.value;
-  previewArea.classList.add("active");
-  button.style.pointerEvents = "auto";
+  previewArea.classList.add('active');
+  button.style.pointerEvents = 'auto';
 
-  if(imgUrl.indexOf("https://www.youtube.com/watch?v=") != -1) {
+  if(imgUrl.indexOf('https://www.youtube.com/watch?v=') != -1) {
     let vidId = imgUrl.split('v=')[1].substring(0, 11),
         ytImgUrl = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
     imgTag.src = ytImgUrl;
-  }else if(imgUrl.indexOf("https://youtu.be/") != -1) {
+  }else if(imgUrl.indexOf('https://youtu.be/') != -1) {
     let vidId = imgUrl.split('be/')[1].substring(0, 11),
         ytImgUrl = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
     imgTag.src = ytImgUrl;
   }else if(imgUrl.match(/\.(jpe?g|png|gif|bmp|webp)$/i)) {
     imgTag.src = imgUrl;
   }else {
-    imgTag.src = "";
-    button.style.pointerEvents = "none";
-    previewArea.classList.remove("active");
+    imgTag.src = '';
+    button.style.pointerEvents = 'none';
+    previewArea.classList.remove('active');
   }
 
   hiddenField.value = imgTag.src;
 };
 
-document.querySelector("form").onsubmit = event =>{
-  event.preventDefault();
-};
+form.onsubmit = event => event.preventDefault();
