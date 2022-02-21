86b61ff83498ba4b94afaaa3cc94a07212ba3990
Mayank Bhurani <99683515+bhurani@users.noreply.github.com>
2022-02-22 01:25:17 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,17 +12,17 @@ urlField.onkeyup = () =>{
   previewArea.classList.add('active');
   button.style.pointerEvents = 'auto';
 
-  if(imgUrl.indexOf('https://www.youtube.com/watch?v=') != -1) {
+  if(imgUrl.indexOf('https://www.youtube.com/watch?v=') != -1){
     let vidId = imgUrl.split('v=')[1].substring(0, 11),
         ytImgUrl = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
     imgTag.src = ytImgUrl;
-  }else if(imgUrl.indexOf('https://youtu.be/') != -1) {
+  }else if(imgUrl.indexOf('https://youtu.be/') != -1){
     let vidId = imgUrl.split('be/')[1].substring(0, 11),
         ytImgUrl = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
     imgTag.src = ytImgUrl;
-  }else if(imgUrl.match(/\.(jpe?g|png|gif|bmp|webp)$/i)) {
+  }else if(imgUrl.match(/\.(jpe?g|png|gif|bmp|webp)$/i)){
     imgTag.src = imgUrl;
-  }else {
+  }else{
     imgTag.src = '';
     button.style.pointerEvents = 'none';
     previewArea.classList.remove('active');
